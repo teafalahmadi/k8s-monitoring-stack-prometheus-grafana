31219f123948ea06af94bb3ac23da87a43c098c9
Ubuntu <azureuser@teaf-vm.q5xcgruxziku3nd5hk3pvwbkxd.milx.internal.cloudapp.net>
2025-10-21 19:53:00 +0000

    Fix: added infra as normal folder instead of submodule

---
+++ infra/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.26.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+
+  features {
+
+  }
+
+  subscription_id = "4421688c-0a8d-4588-8dd0-338c5271d0af"
+}
---
+++ infra/terraform.tfvars
@@ -0,0 +1,3 @@
+prefix = "teafRS-1"
+location = "italynorth"
+default_node_pool_name = "teaf1"
